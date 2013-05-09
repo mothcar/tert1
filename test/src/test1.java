89c41736ebe99f5a38161d39cc001d01e45ed4d9
mothcar <mothar@gmail.com>
2013-05-09 19:18:28 +0900

    first commit

---
+++ test/src/test1.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class test1 {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {	
+
+
+		InputStreamReader str = new InputStreamReader(System.in);
+		BufferedReader str2 = new BufferedReader(str);
+
+		System.out.println("찾고싶은 문자열을 입력하세요");
+		String userStr = str2.readLine();
+
+		System.out.println("당신이 입력한 문자는 "+userStr+" 입니다.");
+
+		BufferedReader b = new BufferedReader(new FileReader("c:/kor.txt"));
+
+
+		String line;
+		boolean tf = false;
+		int i = 1;
+		while ( (line = b.readLine()) != null ) 			  
+		{
+			if(line.equals(userStr)){
+				System.out.println("당신이 찾는 문자 열은 "+i+"라인에 있습니다.");
+				tf= true;
+			}else {					  
+			}				  
+			i++;
+		}
+
+		if(!tf){
+			System.out.println("당신이 찾는 문자열은 없습니다.");
+		}
+	}
+}
